f2902579e56c8f7ee78d06df1ff3f0f76ca651ab
cyb <>
2022-11-23 23:22:57 +0800

    first

---
+++ myjs/click.js
@@ -0,0 +1,79 @@
+const btn = () => {
+    const image_wrapper = document.querySelector('.image_wrapper');
+    image.classList.add('animate__animated', 'animate__fadeInLeft');
+    image_2.classList.add('animate__animated', 'animate__fadeInRight');
+    image_3.classList.add('animate__animated', 'animate__fadeInLeft');
+    image_4.classList.add('animate__animated', 'animate__fadeInLeft');
+    image_5.classList.add('animate__animated', 'animate__fadeInRight');
+    image_6.classList.add('animate__animated', 'animate__fadeInLeft');
+    image_7.classList.add('animate__animated', 'animate__fadeInRight');
+    image_8.classList.add('animate__animated', 'animate__fadeInRight');
+    section_2.classList.add('animate__animated', 'animate__fadeInRight');
+    image_wrapper.classList.add('animate__animated', 'animate__fadeInRight');
+    tap.classList.add('animate__animated', 'animate__fadeInDown');
+    down_icon.classList.add('animate__animated', 'animate__fadeInDown');
+    console.log('11')
+
+}
+const CloudScroll = (event) => {
+    let scrollTop = document.getElementsByClassName('page')[0].scrollTop;
+    if (scrollTop > 20) {
+        image.classList.add('animate__animated', 'animate__fadeOutLeft');
+        image_2.classList.add('animate__animated', 'animate__fadeOutRight');
+        image_3.classList.add('animate__animated', 'animate__fadeOutLeft');
+        image_4.classList.add('animate__animated', 'animate__fadeOutLeft');
+        image_5.classList.add('animate__animated', 'animate__fadeOutRight');
+        image_6.classList.add('animate__animated', 'animate__fadeOutLeft');
+        image_7.classList.add('animate__animated', 'animate__fadeOutRight');
+        image_8.classList.add('animate__animated', 'animate__fadeOutRight');
+        section_2.classList.add('animate__animated', 'animate__fadeOutRight');
+        image_wrapper.classList.add('animate__animated', 'animate__fadeOutRight');
+        tap.classList.add('animate__animated', 'animate__fadeOutDown');
+        down_icon.classList.add('animate__animated', 'animate__fadeOutDown');
+    }
+    if (scrollTop === 0) {
+        image.classList.remove('animate__animated', 'animate__fadeOutLeft');
+        image.classList.add('animate__animated', 'animate__fadeInLeft');
+        image_2.classList.remove('animate__animated', 'animate__fadeOutRight');
+        image_2.classList.add('animate__animated', 'animate__fadeInRight');
+        image_3.classList.remove('animate__animated', 'animate__fadeOutLeft');
+        image_3.classList.add('animate__animated', 'animate__fadeInLeft');
+        image_4.classList.remove('animate__animated', 'animate__fadeOutLeft');
+        image_4.classList.add('animate__animated', 'animate__fadeInLeft');
+        image_5.classList.remove('animate__animated', 'animate__fadeOutRight');
+        image_5.classList.add('animate__animated', 'animate__fadeInRight');
+        image_6.classList.remove('animate__animated', 'animate__fadeOutLeft');
+        image_6.classList.add('animate__animated', 'animate__fadeInLeft');
+        image_7.classList.remove('animate__animated', 'animate__fadeOutRight');
+        image_7.classList.add('animate__animated', 'animate__fadeInRight');
+        image_8.classList.remove('animate__animated', 'animate__fadeOutRight');
+        image_8.classList.add('animate__animated', 'animate__fadeInRight');
+        section_2.classList.remove('animate__animated', 'animate__fadeOutRight');
+        section_2.classList.add('animate__animated', 'animate__fadeInRight');
+        image_wrapper.classList.remove('animate__animated', 'animate__fadeOutRight');
+        image_wrapper.classList.add('animate__animated', 'animate__fadeInRight');
+        tap.classList.remove('animate__animated', 'animate__fadeOutDown');
+        tap.classList.add('animate__animated', 'animate__fadeInDown');
+        down_icon.classList.remove('animate__animated', 'animate__fadeOutDown');
+        down_icon.classList.add('animate__animated', 'animate__fadeInDown');
+    }
+
+}
+
+//IE之外的绑定事件方法 
+if (document.addEventListener && !document.attachEvent) {
+    document.addEventListener('mousewheel', CloudScroll);
+    //FF绑定滚动事件 
+    document.addEventListener('DOMMouseScroll', CloudScroll);
+}
+//IE 
+else if (document.attachEvent && !document.addEventListener) {
+    document.attachEvent('onmousewheel', CloudScroll);
+} else {
+    window.onmousewheel = CloudScroll;
+}
+
+
+const Jump = () => {
+    window.location.href = "slotmachine.html";
+}
---
+++ myjs/slotmachine.js
@@ -0,0 +1,72 @@
+const myLucky = new LuckyCanvas.SlotMachine('#my-lucky', {
+    width: '400px',
+    height: '170px',
+    blocks: [{
+        imgs: [{
+            src: "https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/63324b315a7e3f0310665513/6373beb18d7d66001180ae52/a52c9cc28c429420c2df64197ed7e06b.png",
+            width: '100%',
+            height: '100%'
+        }]
+    }],
+    slots: [
+        { direction: -1 },
+        { speed: 20 },
+    ],
+    prizes: [
+        {
+            imgs: [{
+                width: '100%',
+                top: '20%',
+                src: "https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/63324b315a7e3f0310665513/6373beb18d7d66001180ae52/9ef1805e19139b822bec214abbff5be9.png"
+
+
+            }]
+        },
+        {
+
+            imgs: [{
+                width: '80%',
+                src: "https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/63324b315a7e3f0310665513/6373beb18d7d66001180ae52/b11cffe380bed10c38409b28ac9738d1.png"
+
+            }]
+        },
+        {
+
+            imgs: [{
+                width: '100%',
+                top: '20%',
+                src: "https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/63324b315a7e3f0310665513/6373beb18d7d66001180ae52/9ed0330f40714a42a81a1bbc1a2a1a1f.png"
+
+
+            }]
+        },
+    ],
+    defaultConfig: {
+        rowSpacing: '30px',
+        colSpacing: '80px'
+    }
+
+})
+
+const playGame = () => {
+    myLucky.play()
+    setTimeout(() => {
+        // 假设 4 种结果
+        const res = [
+            [9, 9, 6],
+            [0, 0, 7],
+            [6, 6, 6],
+            [8, 8, 8]
+        ]
+        // 随机取一组数据
+        const index = res[Math.random() * 2 >> 0]
+        // 调用 stop 方法停止游戏
+        myLucky.stop(index[0])
+    }, 500);
+}
+// 循环演示
+playGame()
+setInterval(() => {
+    playGame()
+}, 6000)
+console.log(myLucky)
