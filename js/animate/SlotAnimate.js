29408b76264fa0428ca25d47de832493ddf7e0e9
cyb <>
2022-11-26 19:58:39 +0800

    slot

--- js/animate/SlotAnimate.js
+++ js/animate/SlotAnimate.js
@@ -1,5 +1,20 @@
 
+const image = document.querySelector('.image');
 const image_bottle = document.querySelector('.image_bottle');
 const image_role = document.querySelector('.image_role');
+const image_bottombg = document.querySelector('.image_bottombg');
+const synthetic = document.querySelector('.synthetic')
+const myluck_cti = document.querySelector('.myluck_cti');
+const mix_container = document.querySelector('.mix_container');
+
+
 image_bottle.classList.add('animate__animated', 'animate__fadeInUp');
 image_role.classList.add('animate__animated', 'animate__fadeInUp');
+synthetic.classList.add('animate__animated', 'animate__fadeInUp');
+image.classList.add('animate__animated', 'animate__fadeInDown');
+// image_bottombg.classList.add('animate__animated', 'animate__fadeInUp');
+myluck_cti.classList.add('animate__animated', 'animate__fadeInRight');
+mix_container.classList.add('animate__animated', 'animate__fadeInDown');
+
+
+
--- js/slotmachine.js
+++ js/slotmachine.js
@@ -9,8 +9,13 @@ const myLucky = new LuckyCanvas.SlotMachine('#my-lucky', {
         }]
     }],
     slots: [
-        { direction: -1 },
-        { speed: 20 },
+
+        { order: [0, 1, 2, 3], direction: -1 },
+        { order: [0, 2, 1, 3], speed: 20 },
+
+
+
+
     ],
     prizes: [
         {
@@ -18,28 +23,32 @@ const myLucky = new LuckyCanvas.SlotMachine('#my-lucky', {
                 width: '100%',
                 top: '20%',
                 src: "https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/63324b315a7e3f0310665513/6373beb18d7d66001180ae52/9ef1805e19139b822bec214abbff5be9.png"
-
-
             }]
         },
         {
-
-            imgs: [{
-                width: '80%',
-                src: "https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/63324b315a7e3f0310665513/6373beb18d7d66001180ae52/b11cffe380bed10c38409b28ac9738d1.png"
-
-            }]
-        },
-        {
-
             imgs: [{
                 width: '100%',
-                top: '20%',
+                top: '15%',
                 src: "https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/63324b315a7e3f0310665513/6373beb18d7d66001180ae52/9ed0330f40714a42a81a1bbc1a2a1a1f.png"
+            }]
+        },
+        {
+            imgs: [{
+                width: '90%',
+                src: "https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/63324b315a7e3f0310665513/6373beb18d7d66001180ae52/8179277b95dff8ff8a41931054e0aeac.png"
+
+            }]
+        },
+        {
+            imgs: [{
+                width: '100%',
+                top: '10%',
+                src: "https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/63324b315a7e3f0310665513/6373beb18d7d66001180ae52/d146a2e27d3ef2360f355854b4dbbbf2.png"
 
 
             }]
         },
+
     ],
     defaultConfig: {
         rowSpacing: '30px',
@@ -50,25 +59,46 @@ const myLucky = new LuckyCanvas.SlotMachine('#my-lucky', {
 
 const playGame = () => {
     myLucky.play()
-    setTimeout(() => {
-        // 假设 4 种结果
-        const res = [
-            [9, 9, 6],
-            [0, 0, 7],
-            [6, 6, 6],
-            [8, 8, 8]
-        ]
-        // 随机取一组数据
-        const index = res[Math.random() * 2 >> 0]
-        // 调用 stop 方法停止游戏
-        myLucky.stop(index[0])
-    }, 500);
 }
-// 循环演示
 playGame()
-setInterval(() => {
-    playGame()
-}, 6000)
 console.log(myLucky)
+const stopGame = () => {
+    const synthetic_text = document.querySelector('.synthetic_text')
+    const res = [
+        [0, 1],
+        [2, 3],
+    ]
+    // 随机取一组数据
+    const index = res[Math.random() * 2 >> 0]
 
+    // 调用 stop 方法停止游戏
+    myLucky.stop(index)
+    index.forEach((item, index) => {
+        if (item == 0) {
+            setTimeout(() => {
+                synthetic_text.innerHTML = '香蕉和蛋黄酱'
+
+            }, 4000)
+        }
+        if (item == 2) {
+            setTimeout(() => {
+                synthetic_text.innerHTML = '鸡排和冰淇淋'
+            }, 4000)
+        }
+    })
+    // console.log(index)
+    // console.log(index[0] === 'banner')
+    // if (index[0] === 'banner') {
+    //     console.log('恭喜你中奖了')
+    // }
+    // const mix_container = document.querySelector('.mix_container');
+    // mix_container.classList.add('animate__animated', 'animate__fadeOutDown');
+    // synthetic_text.innerHTML = 'マッチするかな?'
+    // setTimeout(() => {
+
+    //     window.location.href = '/view/chicken.html'
+
+    // }, 4500)
+
+}
 
