1180812239eb6c8dea4b538973d504457d3c97aa
cyb <>
2022-11-27 01:06:18 +0800

    fix

--- js/slotmachine.js
+++ js/slotmachine.js
@@ -10,11 +10,10 @@ const myLucky = new LuckyCanvas.SlotMachine('#my-lucky', {
     }],
     slots: [
 
-        // { order: [0, 1, 2, 3, 4, 5, 6, 7], direction: -1 },
-        // { order: [6, 7, 5, 4, 3, 2, 1, 0], speed: 20 },
-
-        { order: [0, 1, 2, 3, 4, 5], direction: -1, speed: 20 },
-        { order: [2, 1, 3, 0, 5, 4], speed: 20 },
+        { order: [0, 1, 2, 3, 4, 5, 6, 7], speed: 20, direction: -1 },
+        { order: [6, 7, 5, 4, 3, 2, 1, 0], speed: 20 },
+        // { order: [0, 1, 2, 3], direction: -1, speed: 20 },
+        // { order: [2, 1, 3, 0], speed: 20 },
 
     ],
     prizes: [
@@ -75,24 +74,24 @@ const myLucky = new LuckyCanvas.SlotMachine('#my-lucky', {
 
             }]
         },
-        // {
-        //     id: 6,
-        //     imgs: [{
-        //         tips: 'miso',
-        //         width: '100%',
-        //         top: '20%',
-        //         src: "https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/63324b315a7e3f0310665513/6373beb18d7d66001180ae52/c889f68062c6aee24c21421cf0c0ed3d.png"
-        //     }]
-        // },
-        // {
-        //     id: 7,
-        //     imgs: [{
-        //         tips: 'yogurt',
-        //         width: '85%',
-        //         top: '15%',
-        //         src: "https://vanilla-mismatch.vercel.app/assets/Image/yogurt.png"
-        //     }]
-        // },
+        {
+            id: 6,
+            imgs: [{
+                tips: 'miso',
+                width: '100%',
+                top: '20%',
+                src: "https://codefun-proj-user-res-1256085488.cos.ap-guangzhou.myqcloud.com/63324b315a7e3f0310665513/6373beb18d7d66001180ae52/c889f68062c6aee24c21421cf0c0ed3d.png"
+            }]
+        },
+        {
+            id: 7,
+            imgs: [{
+                tips: 'yogurt',
+                width: '85%',
+                top: '15%',
+                src: "https://vanilla-mismatch.vercel.app/assets/Image/yogurt.png"
+            }]
+        },
 
 
     ],
@@ -114,12 +113,12 @@ const stopGame = () => {
         [0, 1],
         [2, 3],
         [4, 5],
-        // [6, 7],
+        [6, 7],
 
     ]
     // 随机取一组数据
 
-    const group = res[Math.random() * 2 >> 0]
+    const group = res[Math.random() * 4 >> 0]
 
 
     // 调用 stop 方法停止游戏
@@ -148,13 +147,13 @@ const stopGame = () => {
             }, 4500)
             window.removeEventListener("click", stopGame, false);
         }
-        // if (item == 6) {
-        //     synthetic_text.innerHTML = 'マッチするかな?'
-        //     setTimeout(() => {
-        //         window.location.href = '/view/miso.html'
-        //     }, 4500)
-        //     window.removeEventListener("click", stopGame, false);
-        // }
+        if (item == 6) {
+            synthetic_text.innerHTML = 'マッチするかな?'
+            setTimeout(() => {
+                window.location.href = '/view/miso.html'
+            }, 4500)
+            window.removeEventListener("click", stopGame, false);
+        }
     })
 
 
