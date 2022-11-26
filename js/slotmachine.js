afd6cceb2aa8547a87fa3013343bdfbd6d881196
cyb <>
2022-11-27 00:43:51 +0800

    fix

--- js/slotmachine.js
+++ js/slotmachine.js
@@ -13,8 +13,8 @@ const myLucky = new LuckyCanvas.SlotMachine('#my-lucky', {
         // { order: [0, 1, 2, 3, 4, 5, 6, 7], direction: -1 },
         // { order: [6, 7, 5, 4, 3, 2, 1, 0], speed: 20 },
 
-        { order: [0, 1, 2, 3], direction: -1, speed: 20 },
-        { order: [2, 1, 3, 0], speed: 20 },
+        { order: [0, 1, 2, 3, 4, 5], direction: -1, speed: 20 },
+        { order: [2, 1, 3, 0, 5, 4], speed: 20 },
 
     ],
     prizes: [
